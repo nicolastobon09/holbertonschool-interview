249bdbebf659040a8d656519170269ecf965f1cc
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-01-25 22:08:30 -0500

    add function that reads stdin line by line and computes metrics

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+import re
+
+
+class LogParsing:
+    # const
+    CODES = {}
+
+    # General variables
+    step = 0
+    total_size = 0
+    status = ""
+    file_size = ""
+
+    def __init__(self):
+        """" constructor """
+        self.run()
+
+    def run(self):
+        """"
+        function that search the status code and size number
+        """
+        while True:
+            try:
+                line = input()
+
+                self.parser(line)
+                self.step += 1
+            except (KeyboardInterrupt, EOFError):
+                self.print_log_parsing()
+                exit()
+
+    def parser(self, line):
+        """"
+        parsing lines
+        Args:
+            line: input from stdin
+        """
+        PATTERN = '([\\d]{3})\\s([\\d]{1,4})$'
+        STOP = 9
+
+        self.status, self.file_size = re.search(PATTERN, line).group().split()
+
+        self.total_size += int(self.file_size)
+
+        try:
+            if self.CODES[self.status]:
+                self.CODES[self.status] += 1
+        except KeyError:
+            self.CODES[self.status] = 1
+
+        if self.step == STOP:
+            self.print_log_parsing()
+            self.step = 0
+
+    def print_log_parsing(self):
+        """
+        function that print parsing logs
+        """
+        print("File size: {}".format(self.total_size - int(self.file_size)))
+        for key, value in sorted(self.CODES.items()):
+            print("{}: {}".format(key, value))
+
+
+if __name__ == '__main__':
+    init = LogParsing()
+    init.run()
