407d5757935f887973da8da040074c220a9051b6
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-01-27 23:06:56 -0500

    [add] prototype function that checks if a node is a root

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,43 +1,54 @@
 #!/usr/bin/python3
-"""Script that reads stdin line by line and computes metrics"""
+""" Class LogParsing """
+import re
 import sys
 
 
-def print_metrics(status_codes, file_size):
-    """function to print status codes and total file size"""
+def print_log_parsing(CODES, file_size):
+    """
+    function that print parsing logs
+    args:
+        codes: is a dictionary of status code
+        file_size: is the size of status
+    """
     print("File size: {}".format(file_size))
-    for k, v in sorted(status_codes.items()):
-        if v > 0:
-            print("{}: {}".format(k, v))
+    for key, value in sorted(CODES.items()):
+        print("{}: {}".format(key, value))
 
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
-counter = 1
-file_size = 0
-try:
-    for line in sys.stdin:
-        splited_line = line.strip().split()
-        if len(splited_line) < 7:
-            continue
-        file_size += int(splited_line[-1])
-        status_code = splited_line[-2]
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-        if counter == 10:
-            print_metrics(status_codes, file_size)
-            counter = 1
-        else:
-            counter += 1
-    print_metrics(status_codes, file_size)
-except KeyboardInterrupt:
-    print_metrics(status_codes, file_size)
-    raise
+def run():
+    """"
+    function that search the status code and size number
+    """
+    PATTERN = '([\\d]{3})\\s([\\d]{1,4})$'
+    CODES = {}
+    STOP = 10
+    step = 1
+    size = 0
+
+    try:
+        for line in sys.stdin:
+
+            status, file_size = re.search(PATTERN, line).group().split()
+
+            try:
+                if CODES[status]:
+                    CODES[status] += 1
+            except KeyError:
+                CODES[status] = 1
+
+            size += int(file_size)
+
+            if step == STOP:
+                print_log_parsing(CODES, size)
+                step = 1
+
+            step += 1
+
+    except (KeyboardInterrupt, EOFError):
+        print_log_parsing(CODES, size)
+        exit()
+
+
+if __name__ == '__main__':
+    run()
