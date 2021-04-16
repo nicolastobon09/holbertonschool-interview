c9b02612a12fd6428da524034afe75e2f6f93f4c
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-15 22:44:51 -0500

    add file

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -6,30 +6,23 @@ rain function
 
 def rain(walls):
     """
-    function that calculate how much water will be retained,
-    after it rains
+    Return: Integer indicating total amount of rainwater retained.
+    If the list is empty return 0.
     """
-    if not walls:
-        return 0
+    n = len(walls)
+    water = 0
 
-    water_retained = 0
-    top_right_wall = 0
-    top_left_wall = 0
+    for i in range(1, n - 1):
+        # Max element on its left
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
 
-    idx = 0
-    size = len(walls) - 1
+        # Max element on its right
+        right = walls[i]
+        for k in range(i + 1, n):
+            right = max(right, walls[k])
 
-    while (idx <= size):
-        if walls[idx] < walls[size]:
-            if walls[idx] > top_left_wall:
-                top_left_wall = walls[idx]
-            else:
-                water_retained += top_left_wall - walls[idx]
-            idx += 1
-        else:
-            if walls[size] > top_right_wall:
-                top_right_wall = walls[size]
-            else:
-                water_retained += top_right_wall - walls[idx]
-            size -= 1
-    return water_retained
+        water += min(left, right) - walls[i]
+
+    return water
